31a18d262c606146fc7641c7af60e839ffd14d78
Avi <12901566+gaviral@users.noreply.github.com>
2025-05-22 12:57:07 -0700

    chore: Update sidebar components after dependency clean

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SidebarOpener from './SidebarOpener';
 
 interface SidebarProps {
@@ -6,40 +6,54 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ position }) => {
+    const [isHidden, setIsHidden] = useState(true);
+
     let bgColorClass = '';
     let borderColorClass = '';
     let textColorClass = '';
     let positionClasses = '';
+    let transitionClasses = 'transition-all duration-300 ease-in-out';
+    let hiddenPositionClasses = '';
 
     switch (position) {
         case 'left':
             bgColorClass = 'bg-blue-200';
             borderColorClass = 'border-blue-700';
             textColorClass = 'text-blue-800';
-            positionClasses = 'fixed top-0 left-0 h-screen w-64 rounded-r-2xl';
+            positionClasses = 'fixed top-0 h-screen w-64';
+            hiddenPositionClasses = '-left-64';
             break;
         case 'bottom':
             bgColorClass = 'bg-green-200';
             borderColorClass = 'border-green-700';
             textColorClass = 'text-green-800';
-            positionClasses = 'fixed bottom-0 left-0 right-0 h-48 rounded-t-2xl';
+            positionClasses = 'fixed left-0 right-0 h-48';
+            hiddenPositionClasses = '-bottom-48';
             break;
         case 'right':
             bgColorClass = 'bg-red-200';
             borderColorClass = 'border-red-700';
             textColorClass = 'text-red-800';
-            positionClasses = 'fixed top-0 right-0 h-screen w-64 rounded-l-2xl';
+            positionClasses = 'fixed top-0 h-screen w-64';
+            hiddenPositionClasses = '-right-64';
             break;
         default:
             bgColorClass = 'bg-gray-200';
             borderColorClass = 'border-gray-700';
             textColorClass = 'text-gray-800';
             positionClasses = '';
+            hiddenPositionClasses = '';
     }
 
+    const currentPositionClass = isHidden ? hiddenPositionClasses : (position === 'left' ? 'left-0' : (position === 'right' ? 'right-0' : (position === 'bottom' ? 'bottom-0' : '')));
+
     return (
-        <div className={`sidebar p-4 border-4 flex justify-center items-center ${bgColorClass} ${borderColorClass} ${textColorClass} ${positionClasses}`}>
-            <SidebarOpener parentSidebarPosition={position} />
+        <div
+            className={`sidebar p-4 border-4 flex justify-center items-center ${bgColorClass} ${borderColorClass} ${textColorClass} ${positionClasses} ${currentPositionClass} ${transitionClasses}`}
+            onMouseEnter={() => setIsHidden(false)}
+            onMouseLeave={() => setIsHidden(true)}
+        >
+            <SidebarOpener parentSidebarPosition={position} setIsParentSidebarHidden={setIsHidden} />
             {position} sidebar
         </div>
     );
--- frontend/src/components/SidebarOpener.tsx
+++ frontend/src/components/SidebarOpener.tsx
@@ -38,7 +38,9 @@ const SidebarOpener: React.FC<SidebarOpenerProps> = ({ parentSidebarPosition }) 
     }
 
     return (
-        <div style={openerStyle}></div>
+        <div
+            style={openerStyle}
+        ></div>
     );
 };
 
